7c7fe39b557a9d65fb00e8eb27c7b22d5c07d176
Muhammad Kashif <91327646+MK61395@users.noreply.github.com>
2023-06-13 15:45:31 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Header.h"
+using namespace std;
+
+int main()
+{
+	Manager m1;
+	//m1.Login();
+	m1.Menu();
+
+	//Customer c1;
+	//c1.Menu();
+	/*int option;
+	cout << "1. Admin" << endl;
+	cout << "2. Manager" << endl;
+	cout << "3. Customer" << endl;
+	cin >> option;
+	if (option == 1)
+	{
+		Admin a1;
+		a1.Register();
+	}
+	if (option == 2)
+	{
+		Manager m1;
+		m1.Register();
+	}
+	if (option == 3)
+	{
+		Customer c1;
+		c1.Register();
+	}*/
+
+
+
+	//Customer c1;
+	//c1.Register();
+
+	//c1.Login();
+
+	//Admin a1;
+	//a1.Register();
+	//a1.Login();
+	//a1.Menu();
+	//a1.registerManager();
+	//ProductCatalog p;
+
+	//ProductCatalog p1;
+	//p1.AddItems_W("Washing powder");
+
+	//Admin a1;
+	//a1.removeSpam();
+	//a1.addProducts();
+	//a1.manageStores();
+	//a1.removeProducts();
+	//a1.updateProducts();
+	//cout << "Chicken" << "\t" << 650 << endl;
+
+
+
+	//Manager m1;
+	//m1.Login();
+}
